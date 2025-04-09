e7e1af2b37aba374e22118caa08e73b5d5f8156b
DevBaldo <135394363+DevBaldo@users.noreply.github.com>
2025-04-08 18:36:46 -0600

    Exemen comit inicial

---
+++ DAL/Implementations/UnidadDeTrabajo.cs
@@ -0,0 +1,50 @@
+﻿using DAL.Interfaces;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementations
+{
+    public class UnidadDeTrabajo : IUnidadDeTrabajo
+    {
+        public ICategoryDAL CategoryDAL { get; set; }
+        public ISupplierDAL SupplierDAL { get; set; }
+        public IProductDAL ProductDAL { get; set; }
+
+        private NorthWindContext _northWindContext;
+
+        public UnidadDeTrabajo(NorthWindContext northWindContext,
+                        ICategoryDAL categoryDAL, ISupplierDAL supplierDAL, IProductDAL productDAL
+                       
+            ) 
+        {
+                this._northWindContext = northWindContext;
+                this.CategoryDAL = categoryDAL; 
+                this.SupplierDAL = supplierDAL;
+                this.ProductDAL = productDAL;
+        }
+       
+
+        public bool Complete()
+        {
+            try
+            {
+                _northWindContext.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public void Dispose()
+        {
+            this._northWindContext.Dispose();
+        }
+    }
+}
---
+++ DAL/Interfaces/IUnidadDeTrabajo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IUnidadDeTrabajo: IDisposable
+    {
+        ICategoryDAL CategoryDAL { get; }
+        ISupplierDAL SupplierDAL { get; } 
+        
+        IProductDAL ProductDAL { get; }
+       
+
+        bool Complete();
+    }
+}
